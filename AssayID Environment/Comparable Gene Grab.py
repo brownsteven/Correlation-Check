06a7d71d1c524c37e314e0a154f9be70f2a7ee9e
Steven Brown <snbrown3@gmail.com>
2017-05-08 14:58:04 -0400

    Update Comparable Gene Grab.py

--- AssayID Environment/Comparable Gene Grab.py
+++ AssayID Environment/Comparable Gene Grab.py
@@ -4,23 +4,30 @@ from openpyxl import load_workbook
 def similarCheck():
     wb = load_workbook(filename='test input file.xlsx')
     ws = wb['Sheet1']
+    
+    with open('Test output.csv', 'w') as outcsv:
+        writer = csv.writer(outcsv)
+        writer.writerow(['Percentage Match', 'Gene A', 'Assay ID A', 
+                         'Gene B', 'Assay ID B'])
+        for row in ws.rows:
+            col = 0
+            for cell in row:
+                col += 1
+                if cell.value == 1:
+                    return
+                elif type(cell.value) == float:
+                    #if cell.value > .9 and cell.value < 1:
+                    if 0.9 <= cell.value < 1:
+                        # Make a .csv file with the following:
+                        """with open('Test output.csv', 'w') as outcsv:
+                            writer = csv.writer(outcsv)
+                            writer.writerow(['Percentage Match', 'Gene A', 'AssayID A',
+                                             'Gene B', 'Assay ID B'])"""
 
-    for row in ws.rows:
-        c = 0
-        # Look up how to set end points or max cell here. Maybe only do the bottom half
-        for cell in row:
-            c += 1
-            if type(cell.value) == float:
-                if cell.value > .9 and cell.value < 1:
-                    # Make a .csv file with the following:
-                    """with open('Test output.csv', 'w') as outcsv:
-                        writer = csv.writer(outcsv)
-                        writer.writerow(['Percentage', 'Gene', 'AssayID'])"""
-
-                    print('Percentage:', cell.value, 'Gene:',
-                          row[0].value, ws.cell(row=1, column=c).value,
-                          'AssayID:', row[1].value,
-                          ws.cell(row=2, column=c).value)
+                        print('Percentage:', cell.value, 'Gene:',
+                              row[0].value, ws.cell(row=1, column=c).value,
+                              'AssayID:', row[1].value,
+                              ws.cell(row=2, column=c).value)
 
 
 similarCheck()
