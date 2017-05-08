c5d02f001e28f13a6050b1050e361eb10d347122
Steven Brown <snbrown3@gmail.com>
2017-05-08 16:12:41 -0400

    Update Comparable Gene Grab.py

--- AssayID Environment/Comparable Gene Grab.py
+++ AssayID Environment/Comparable Gene Grab.py
@@ -1,33 +1,31 @@
 from openpyxl import load_workbook
+import csv
 
 
 def similarCheck():
     wb = load_workbook(filename='test input file.xlsx')
     ws = wb['Sheet1']
-    
+
     with open('Test output.csv', 'w') as outcsv:
         writer = csv.writer(outcsv)
-        writer.writerow(['Percentage Match', 'Gene A', 'Assay ID A', 
+        writer.writerow(['Percentage Match', 'Gene A', 'Assay ID A',
                          'Gene B', 'Assay ID B'])
         for row in ws.rows:
             col = 0
             for cell in row:
                 col += 1
                 if cell.value == 1:
-                    return
+                    break
                 elif type(cell.value) == float:
-                    #if cell.value > .9 and cell.value < 1:
+                    # if cell.value > .9 and cell.value < 1:
                     if 0.9 <= cell.value < 1:
-                        # Make a .csv file with the following:
-                        """with open('Test output.csv', 'w') as outcsv:
-                            writer = csv.writer(outcsv)
-                            writer.writerow(['Percentage Match', 'Gene A', 'AssayID A',
-                                             'Gene B', 'Assay ID B'])"""
+                        writer.writerow([cell.value, row[0].value, row[1].value,
+                                         ws.cell(row=1, column=col).value, ws.cell(row=2, column=col).value])
 
                         print('Percentage:', cell.value, 'Gene:',
-                              row[0].value, ws.cell(row=1, column=c).value,
+                              row[0].value, ws.cell(row=1, column=col).value,
                               'AssayID:', row[1].value,
-                              ws.cell(row=2, column=c).value)
+                              ws.cell(row=2, column=col).value)
 
 
 similarCheck()
